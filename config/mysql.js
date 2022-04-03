8390973d00faed3179d2432562dfdb579220ea90
yumin <dorayuminlee@gmail.com>
2022-04-04 03:52:48 +0900

    [chore] new relic config

--- config/mysql.js
+++ config/mysql.js
@@ -3,6 +3,8 @@ const mysql = require("mysql");
 const mysqlConnection = {
   init: function () {
     return mysql.createPool({
+      app_name: process.env.NEW_RELIC_APP_NAME,
+      license_key: process.env.NEW_RELIC_LICENSE_KEY,
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
